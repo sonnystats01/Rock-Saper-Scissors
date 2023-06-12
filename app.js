d0d14c8c1dc0972780eb0f1626e527592f67ad29
Sonny Patti <sonnystats01@gmail.com>
2023-06-12 13:05:43 -0600

    Thank goodness for chat gpt The hardest thing was the comparing the choices with a button click.

--- app.js
+++ app.js
@@ -1,67 +1,58 @@
-
-
-//creating a random number and then assigning said numbers to a string value of rock paper or scissors
-function getComputerChoice () {
-    let randChoice = Math.floor(Math.random() * 3 + 1);
-    if(randChoice === 1) {
-        return 'rock';
-    } else if (randChoice === 2) {
-        return 'paper';
-    } else if (randChoice === 3) {
-        return  'scissors';
-    }
-
-};
-
-//using a funciton to get the userinput and determining if it's one of the 3 options 
-const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-        return userInput;
-    } else {
-        console.log('Not a valid selection. Please try again.') 
-    }
-}
-// using a function to determine the winner between the computer choice and the getUserChoice
-//returning is a powerful piece of syntax as you can use the value later on. Very useful to know
-function determineWinner (getUserChoice, getComputerChoice) {
-    if(getUserChoice === getComputerChoice) {
-        return 'This game was a tie!'
-    }
-    if(getUserChoice === 'rock') {
-        if(getComputerChoice === 'paper') {
-            return 'You lose to the computer!'
-        } else {
-            return 'You beat the computer!'
-        }
-    }
-
-    if(getUserChoice === 'paper') {
-        if(getComputerChoice === 'scissors'){
-            return 'You lose to the computer!'
-        } else {
-            return 'You beat the computer!'
-        }
-    }
-
-    if(getUserChoice === 'scissors') {
-        if (getComputerChoice === 'rock') {
-            return 'You lose to the computer'
-        } else {
-            return 'You beat the computer!'
-        }
-    }
+// Function to generate computer's choice
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
+// Function to play a single round of the game
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
 
-//the final part of putting it all together. Wasn't totally sure on how to add a scoring element. But may try later on. 
-function playGame(){
-    let userChoice = getUserChoice('paper');
-    let computerChoice = getComputerChoice();
-    console.log(`You shot a ${userChoice}`);
-    console.log(`The computer shot a ${computerChoice}`);
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
 
+// Function to update the game results on the web page
+function updateResults(result, playerScore, computerScore) {
+  const resultText = document.getElementById("result");
+  const scoreText = document.getElementById("score");
+
+  resultText.textContent = result;
+  scoreText.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
 }
-for(let i = 0; i <= 5; i++) {
-    playGame();
+
+// Function to start the game
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  const buttons = document.querySelectorAll(".choice");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const playerSelection = this.dataset.choice;
+      const computerSelection = computerPlay();
+      const result = playRound(playerSelection, computerSelection);
+
+      if (result === "You win!") {
+        playerScore++;
+      } else if (result === "You lose!") {
+        computerScore++;
+      }
+
+      updateResults(result, playerScore, computerScore);
+    });
+  });
 }
+
+// Start the game
+game();
